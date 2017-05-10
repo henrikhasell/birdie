5c39c668b9c2ab2eb3a09feb736c43cc7c1c7856
Henrik Hasell <henrikhasell@gmail.com>
2017-05-10 08:08:16 +0100

    Added table creation.

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h>
+
+void create(sqlite3 *database)
+{
+    char *error;
+
+    const char *query = "CREATE TABLE connections("
+    "i INTEGER PRIMARY KEY NOT NULL,"
+    "address INTEGER NOT NULL);";
+
+    if(sqlite3_exec(database, query, NULL, NULL, &error) != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to create database: %s\n", error);
+        sqlite3_free(error);
+    }
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -8,11 +26,13 @@ int main(int argc, char *argv[])
 
     if(result == SQLITE_OK)
     {
-        
+	create(database);
+        sqlite3_close(database);
     }
     else
     {
-
+        const char *error = sqlite3_errmsg(database);
+	fprintf(stderr, "SQLite encountered an error: %s\n", error);
     }
 
     return EXIT_SUCCESS;
