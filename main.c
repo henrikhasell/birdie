3e3bcb395b55fbd0b6f2f3b2393caa00d8a3dbc1
Henrik Hasell <henrik@gamingrealms.com>
2017-05-11 17:50:45 +0100

    Added allocateString method.

--- main.c
+++ main.c
@@ -6,9 +6,84 @@
 #include <unistd.h>
 #include <netdb.h>
 
-void create(sqlite3 *database)
+char *allocateString(const char format[], ...)
 {
-    const char *query = "CREATE TABLE connections("
+    va_list arguments1;
+    va_list arguments2;
+
+    va_start(arguments1, format);
+    va_start(arguments2, format);
+
+    int size = vsnprintf(NULL, 0, format, arguments1) + 1;
+
+    char *output = malloc(size);
+
+    if(output)
+    {
+	*output = '\0';
+        vsprintf(output, format, arguments2);
+    }
+
+    va_end(arguments1);
+    va_end(arguments2);
+
+    return output;
+}
+
+char *ipv4ToString(const struct sockaddr_in *address)
+{
+   return allocateString("%u.%u.%u.%u\n",
+        ((char*)&address->sin_addr)[0],
+        ((char*)&address->sin_addr)[1],
+        ((char*)&address->sin_addr)[2],
+        ((char*)&address->sin_addr)[3]
+    );
+}
+
+char *ipv6ToString(const struct sockaddr_in6 *address)
+{
+    return allocateString(
+        "%02x%02x:%02x%02x:%02x%02x:%02x%02x:"
+        "%02x%02x:%02x%02x:%02x%02x:%02x%02x\n",
+        address->sin6_addr.s6_addr[0],
+        address->sin6_addr.s6_addr[1],
+        address->sin6_addr.s6_addr[2],
+        address->sin6_addr.s6_addr[3],
+        address->sin6_addr.s6_addr[4],
+        address->sin6_addr.s6_addr[5],
+        address->sin6_addr.s6_addr[6],
+        address->sin6_addr.s6_addr[7],
+        address->sin6_addr.s6_addr[8],
+        address->sin6_addr.s6_addr[9],
+        address->sin6_addr.s6_addr[10],
+        address->sin6_addr.s6_addr[11],
+        address->sin6_addr.s6_addr[12],
+        address->sin6_addr.s6_addr[13],
+        address->sin6_addr.s6_addr[14],
+        address->sin6_addr.s6_addr[15]
+    );
+}
+
+char *ipToString(const struct sockaddr_storage *address)
+{
+    if(address->ss_family == AF_INET)
+    {
+        return ipv4ToString((struct sockaddr_in*)address);
+    }
+
+    if(address->ss_family == AF_INET6)
+    {
+        return ipv6ToString((struct sockaddr_in6*)address);
+    }
+
+    fputs("Invalid address type.", stderr);
+
+    return NULL;
+}
+
+void createTable(sqlite3 *database)
+{
+    const char *query = "CREATE TABLE IF NOT EXISTS connections("
     "i INTEGER PRIMARY KEY NOT NULL,"
     "address INTEGER NOT NULL);";
 
@@ -30,11 +105,11 @@ int main(int argc, char *argv[])
 
     if(result == SQLITE_OK)
     {
-	    create(database);
+	createTable(database);
 
         struct addrinfo hints;
         memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET6;
+        hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_flags = AI_PASSIVE;
 
@@ -83,48 +158,15 @@ int main(int argc, char *argv[])
                     }
                     else
                     {
-                        puts("Received remote connection.");
+                        char *address_string = ipToString(&remote_address);
 
-                        if(remote_address.ss_family == AF_INET)
+                        printf("Received connection from %s\n", address_string);
+
+                        if(address_string)
                         {
-                            struct sockaddr_in *cast =
-                                (struct sockaddr_in*)&remote_address;
-                            printf("%d.%d.%d.%d\n",
-                                ((char*)&cast->sin_addr)[0],
-                                ((char*)&cast->sin_addr)[1],
-                                ((char*)&cast->sin_addr)[2],
-                                ((char*)&cast->sin_addr)[3]
-                            );
+                            free(address_string);
                         }
-                        else if(remote_address.ss_family == AF_INET6)
-                        {
-                            struct sockaddr_in6 *cast =
-                                (struct sockaddr_in6*)&remote_address;
-                            printf(
-                                "%02x%02x:%02x%02x:%02x%02x:%02x%02x:"
-                                "%02x%02x:%02x%02x:%02x%02x:%02x%02x\n",
-                                cast->sin6_addr.s6_addr[0],
-                                cast->sin6_addr.s6_addr[1],
-                                cast->sin6_addr.s6_addr[2],
-                                cast->sin6_addr.s6_addr[3],
-                                cast->sin6_addr.s6_addr[4],
-                                cast->sin6_addr.s6_addr[5],
-                                cast->sin6_addr.s6_addr[6],
-                                cast->sin6_addr.s6_addr[7],
-                                cast->sin6_addr.s6_addr[8],
-                                cast->sin6_addr.s6_addr[9],
-                                cast->sin6_addr.s6_addr[10],
-                                cast->sin6_addr.s6_addr[11],
-                                cast->sin6_addr.s6_addr[12],
-                                cast->sin6_addr.s6_addr[13],
-                                cast->sin6_addr.s6_addr[14],
-                                cast->sin6_addr.s6_addr[15]
-                            );
-                        }
-                        else
-                        {
-                            fputs("Remote has an invalid address.\n", stderr);
-                        }
+
                         close(remote);
                     }
                 }
